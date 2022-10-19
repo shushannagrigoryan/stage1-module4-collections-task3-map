6be13e9038df432a00243af23623bf26db248dbc
Shushanna <shushannagrigoryan7@gmail.com>
2022-10-19 23:11:01 +0400

    5

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,16 +1,13 @@
 package com.epam.mjc.collections.map;
 
 import javax.swing.*;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
-        Map<String, Integer> map = new ConcurrentHashMap<>();
-        sentence = sentence.toLowerCase(Locale.ROOT);
+        Map<String, Integer> map = new LinkedHashMap<>();
+        sentence = sentence.toLowerCase();
         String [] s = sentence.split("");
         for(int i=0; i<s.length;i++){
             s[i] = s[i].replaceAll("[^a-zA-Z]", "");
@@ -18,9 +15,7 @@ public class WordRepetitionMapCreator {
 
         for (String value : s) {
             boolean set = false;
-
             if (!map.isEmpty()) {
-
                 for (Map.Entry<String, Integer> e : map.entrySet()) {
                     if (Objects.equals(e.getKey(), value)) {
                         e.setValue(e.getValue() + 1);
@@ -35,6 +30,7 @@ public class WordRepetitionMapCreator {
                 map.put(value, 1);
             }
         }
+
         return map;
 
     }
