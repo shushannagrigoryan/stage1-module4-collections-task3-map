ffdb8ddbde3592e1aa764e7e7f92f59390b42951
Shushanna <shushannagrigoryan7@gmail.com>
2022-10-19 23:16:31 +0400

    6

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -2,11 +2,12 @@ package com.epam.mjc.collections.map;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
-        Map<String, Integer> newMap = new HashMap<>();
+        Map<String, Integer> newMap = new LinkedHashMap<>();
 
         for(Map.Entry<Integer,String> e :sourceMap.entrySet()){
             newMap.put(e.getValue(), e.getKey());
